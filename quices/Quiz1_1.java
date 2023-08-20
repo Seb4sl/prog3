5d42bc0c410596fe0a3571603ff60b08200ac3f5
Juan Sebastian Larrota <141367323+Seb4sl@users.noreply.github.com>
2023-08-20 00:05:57 -0500

    Update Quiz1_1.java

--- quices/Quiz1_1.java
+++ quices/Quiz1_1.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+
+public class Quiz1_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa un número n: ");
+        
+        int n = scanner.nextInt();
+        fizzBuzz(n);
+    }
+
+    public static void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
 
