9cfdbbd915458ab1b85113a6360aae7e37ab82b5
Juan Sebastian Larrota <141367323+Seb4sl@users.noreply.github.com>
2023-09-06 22:17:05 -0500

    Update

---
+++ labs/02.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.matrices;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jlarr
+ */
+public class MATRICES {
+
+    public static void main(String[] args) {
+      
+    Scanner teclado = new Scanner (System.in);
+    
+    int primerMatriz[][] = new int [3][3];
+    int segundaMatriz[][] = new int [3][3];
+    int sumaResultado[][] = new int [3][3];
+    int restaResultado[][] = new int [3][3];
+    int multResultado[][] = new int [3][3];
+    
+    
+    for(;;){
+        
+        System.out.println("Digite los elementos de la primera matriz");
+        for(int i = 0 ; i<3 ; i ++){
+            for (int j = 0 ; j<3 ; j++){
+                System.out.println("Digite un elemento en la posicion ");
+            }
+        }
+        
+    }
+    }
+}
+
