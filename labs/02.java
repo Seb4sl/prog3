a6c8d17682b003ed69468afb732e3124ebe214ff
Juan Sebastian Larrota <141367323+Seb4sl@users.noreply.github.com>
2023-09-06 22:59:51 -0500

    Update 02.java

--- labs/02.java
+++ labs/02.java
@@ -16,23 +16,76 @@ public class MATRICES {
       
     Scanner teclado = new Scanner (System.in);
     
-    int primerMatriz[][] = new int [3][3];
+    int primeraMatriz[][] = new int [3][3];
     int segundaMatriz[][] = new int [3][3];
     int sumaResultado[][] = new int [3][3];
     int restaResultado[][] = new int [3][3];
     int multResultado[][] = new int [3][3];
     
     
-    for(;;){
-        
         System.out.println("Digite los elementos de la primera matriz");
         for(int i = 0 ; i<3 ; i ++){
             for (int j = 0 ; j<3 ; j++){
-                System.out.println("Digite un elemento en la posicion ");
+                System.out.println("Digite un elemento en la posicion ["+i+"]["+j+"] : ");
+                primeraMatriz[i][j] = teclado.nextInt();
             }
         }
         
-    }
+        System.out.println("");    
+
+        System.out.println("Digite los elementos de la segunda matriz");
+        for(int i = 0 ; i<3 ; i ++){
+            for (int j = 0 ; j<3 ; j++){
+                System.out.println("Digite un elemento en la posicion ["+i+"]["+j+"] : ");
+                segundaMatriz[i][j] = teclado.nextInt();
+            }
+        }
+        
+        for(int i = 0 ; i<primeraMatriz.length ; i++){
+            for(int j = 0 ; j<segundaMatriz.length ; j++){
+                sumaResultado[i][j] = primeraMatriz[i][j] + segundaMatriz [i][j];
+            }
+        }
+        for(int i = 0 ; i<primeraMatriz.length ; i++){
+            for(int j = 0 ; j<segundaMatriz.length ; j++){
+                restaResultado[i][j] = primeraMatriz[i][j] - segundaMatriz [i][j];
+            }
+        }
+        for(int i = 0 ; i<primeraMatriz.length ; i++){
+            for(int j = 0 ; j<segundaMatriz.length ; j++){
+                multResultado[i][j] = primeraMatriz[i][j]* segundaMatriz [i][j];
+            }
+        }
+        
+        System.out.println("");
+        
+        System.out.println("La suma de las matrices es de: ");
+        for(int i = 0 ; i<primeraMatriz.length; i++){
+             for(int j = 0 ; j<primeraMatriz.length; j++){
+                 System.out.print(" [ " +primeraMatriz[i][j] + " ]");   
+             }
+             
+             if(i==1){
+                 System.out.println("       +       ");
+                 
+             }else{
+                 System.out.println("               ");                 
+                 
+             }
+             for(int j = 0 ; j<segundaMatriz.length; j++){
+                 System.out.print(" [ " +primeraMatriz[i][j] + " ]");   
+             }
+             if(i==1){
+                 System.out.println("       =       ");
+                 
+             }else{
+                 System.out.println("               ");                 
+                 
+             }
+              for(int j = 0 ; j<sumaResultado.length; j++){
+                 System.out.print(" [ " +sumaResultado[i][j] + " ]");   
+             }           
+             System.out.println("");
+        }
     }
 }
-
