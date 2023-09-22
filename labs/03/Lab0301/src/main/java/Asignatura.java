056cdb8fe71eabd6a7af6780b9ae213595bb6acb
Juan Sebastian Larrota <141367323+Seb4sl@users.noreply.github.com>
2023-09-21 23:50:07 -0500

    Update Asignatura.java

--- labs/03/Lab0301/src/main/java/Asignatura.java
+++ labs/03/Lab0301/src/main/java/Asignatura.java
@@ -1,7 +1,38 @@
-public class Asignatura {
-    int codigo;
-    String nombre;
-    int semestre;
-    int num_creditos;
-    String horario; 
+class Asignatura 
+{
+    private String Codigo;
+    private String Nombre;
+    private int Semestre;
+    private int Creditos;
+    private List<String> Horario;
+
+    public Asignatura(String Codigo, String Nombre, int Semestre, int Creditos, List<String> Horario) {
+        this.Codigo = Codigo;
+        this.Nombre = Nombre;
+        this.Semestre = Semestre;
+        this.Creditos = Creditos;
+        this.Horario = Horario;
+    }
+
+    public int getCreditos() 
+    {
+        return Creditos;
+    }
+
+    public boolean CruceHorario(Asignatura otraAsignatura) 
+    {
+        for (String diaHora : Horario) 
+        {
+            if (otraAsignatura.Horario.contains(diaHora)) 
+            {
+                return true; // Hay un cruce de horario
+            }
+        }
+        return false; // No hay cruces de horario
+    }
+
+    @Override
+    public String toString() {
+        return "Codigo: " + Codigo + ", Nombre: " + Nombre + ", Semestre: " + Semestre + ", Creditos: " + Creditos;
+    }
 }
