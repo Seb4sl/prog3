2ce4a184e6fdffb3a3782823a8dcd5c228785cc0
Juan Sebastian Larrota <141367323+Seb4sl@users.noreply.github.com>
2023-09-21 23:49:33 -0500

    Update Estudiante.java

--- labs/03/Lab0301/src/main/java/Estudiante.java
+++ labs/03/Lab0301/src/main/java/Estudiante.java
@@ -1,48 +1,92 @@
+    import java.io.*;
+import java.util.*;
 
-public class Estudiante {
-    
-     private String codigo;;
-     private String nombre;
-     private String correo;
-     private int semestre;
+class Estudiante 
+{
+    private String Codigo;
+    private String Nombre;
+    private String Correo;
+    private int Semestre;
+    private List<Asignatura> AsignaturasRegistradas;
+    private int TotalCreditosRegistrados;
 
-    public Estudiante(String codigo, String nombre, String correo, int semestre) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.correo = correo;
-        this.semestre = semestre;
+    public Estudiante(String Codigo, String Nombre, String Correo, int Semestre)
+     {
+        this.Codigo = Codigo;
+        this.Nombre = Nombre;
+        this.Correo = Correo;
+        this.Semestre = Semestre;
+        this.AsignaturasRegistradas = new ArrayList<>();
+        this.TotalCreditosRegistrados = 0;
     }
 
-    public String getCodigo() {
-        return codigo;
+    public void registrarAsignatura(Asignatura asignatura) 
+    {
+        if (TotalCreditosRegistrados + asignatura.getCreditos() <= 16) 
+        {
+            if (!VerificarCruceHorario(asignatura)) 
+            {
+                AsignaturasRegistradas.add(asignatura);
+                TotalCreditosRegistrados += asignatura.getCreditos();
+                System.out.println("Asignatura registrada con exito.");
+            } else 
+            {
+                System.out.println("La asignatura seleccionada tiene un cruce de horario.");
+            }
+        } else 
+        {
+            System.out.println("No puedes registrar mas asignaturas debido al limite de creditos.");
+        }
     }
 
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
+    private boolean VerificarCruceHorario(Asignatura nuevaAsignatura) 
+    {
+        for (Asignatura asignatura : AsignaturasRegistradas) 
+        {
+            if (asignatura.CruceHorario(nuevaAsignatura)) 
+            {
+                return true; // Hay un cruce de horario
+            }
+        }
+        return false; // No hay cruces de horario
     }
 
-    public String getNombre() {
-        return nombre;
+    public void MostrarReporte() 
+    {
+        System.out.println("-------- Reporte de Asignaturas Registradas --------");
+        System.out.println("Codigo: " + Codigo);
+        System.out.println("Nombre: " + Nombre);
+        System.out.println("Correo: " + Correo);
+        System.out.println("Semestre: " + Semestre);
+        System.out.println("Total de creditos registrados: " + TotalCreditosRegistrados);
+        System.out.println("Asignaturas registradas:");
+
+        for (Asignatura asignatura : AsignaturasRegistradas) 
+        {
+            System.out.println(asignatura);
+        }
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    public void GenerarReporteArchivo(String NombreArchivo) 
+    {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(NombreArchivo))) 
+        {
+            writer.println("=== Reporte de Asignaturas Registradas ===");
+            writer.println("Codigo: " + Codigo);
+            writer.println("Nombre: " + Nombre);
+            writer.println("Correo: " + Correo);
+            writer.println("Semestre: " + Semestre);
+            writer.println("Total de creditos registrados: " + TotalCreditosRegistrados);
+            writer.println("Asignaturas registradas:");
 
-    public String getCorreo() {
-        return correo;
+            for (Asignatura asignatura : AsignaturasRegistradas) 
+            {
+                writer.println(asignatura);
+            }
+            System.out.println("Reporte generado con exito en el archivo " + NombreArchivo);
+        } catch (IOException e) 
+        {
+            System.err.println("Error al generar el archivo de reporte.");
+        }
     }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public int getSemestre() {
-        return semestre;
-    }
-
-    public void setSemestre(int semestre) {
-        this.semestre = semestre;
-    }
-    
 }
