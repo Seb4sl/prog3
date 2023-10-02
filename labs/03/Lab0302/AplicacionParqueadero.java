4c3e22d4024f79d641294eed1a0f6daf209587a5
Juan Sebastian Larrota <141367323+Seb4sl@users.noreply.github.com>
2023-10-02 09:27:37 -0500

    Update AplicacionParqueadero.java

--- labs/03/Lab0302/AplicacionParqueadero.java
+++ labs/03/Lab0302/AplicacionParqueadero.java
@@ -12,3 +12,68 @@ public class AplicacionParqueadero extends JFrame {
     private List<Vehiculo> vehiculos = new ArrayList<>();
     private SimpleDateFormat formatoFecha = new SimpleDateFormat("HH:mm:ss");
     private int numeroVehiculo = 1;
+
+    public AplicacionParqueadero() {
+        setTitle("Parqueadero UMNG");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        getContentPane().setBackground(Color.WHITE);
+
+        JMenuBar barraMenu = new JMenuBar();
+        JMenu menu = new JMenu("Menú");
+        JMenuItem itemEntrada = new JMenuItem("Entrada del vehículo");
+        JMenuItem itemSalida = new JMenuItem("Salida del vehículo");
+        JMenuItem itemListaDosRuedas = new JMenuItem("Lista de vehículos con 2 ruedas");
+        JMenuItem itemListaCuatroRuedas = new JMenuItem("Lista de Vehículos de 4 ruedas");
+        JMenuItem itemTotalVehiculos = new JMenuItem("Número de vehículos totales");
+
+        itemEntrada.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                registrarEntradaVehiculo();
+            }
+        });
+
+        itemSalida.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                registrarSalidaVehiculo();
+            }
+        });
+
+        itemListaDosRuedas.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                mostrarListaDosRuedas();
+            }
+        });
+
+        itemListaCuatroRuedas.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                mostrarListaCuatroRuedas();
+            }
+        });
+
+        itemTotalVehiculos.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                mostrarTotalVehiculos();
+            }
+        });
+
+        menu.add(itemEntrada);
+        menu.add(itemSalida);
+        menu.add(itemListaDosRuedas);
+        menu.add(itemListaCuatroRuedas);
+        menu.add(itemTotalVehiculos);
+        barraMenu.add(menu);
+        setJMenuBar(barraMenu);
+
+        JPanel panelEntrada = new JPanel();
+        panelEntrada.setLayout(new GridLayout(3, 2));
+        campoPlaca = new JTextField();
+        campoTipo = new JTextField();
+        JButton botonEntrada = new JButton("Entrada");
+
+        botonEntrada.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                registrarEntradaVehiculo();
+            }
+        });
