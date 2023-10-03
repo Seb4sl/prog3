65d98e4acacd647fd1bba95bccb7cd099a0c0aab
Juan Sebastian Larrota <141367323+Seb4sl@users.noreply.github.com>
2023-10-02 19:28:22 -0500

    Update AplicacionParqueadero.java

--- labs/03/Lab0302/AplicacionParqueadero.java
+++ labs/03/Lab0302/AplicacionParqueadero.java
@@ -77,3 +77,161 @@ public class AplicacionParqueadero extends JFrame {
                 registrarEntradaVehiculo();
             }
         });
+
+        panelEntrada.add(new JLabel("Placa:"));
+        panelEntrada.add(campoPlaca);
+        panelEntrada.add(new JLabel("Tipo de vehículo:"));
+        panelEntrada.add(campoTipo);
+        panelEntrada.add(new JLabel(""));
+        panelEntrada.add(botonEntrada);
+
+        areaRegistro = new JTextArea();
+        areaRegistro.setEditable(false);
+        areaRegistro.setFont(new Font("Monospaced", Font.PLAIN, 12));
+        areaRegistro.setBackground(Color.LIGHT_GRAY);
+        JScrollPane panelDesplazamiento = new JScrollPane(areaRegistro);
+
+        add(panelEntrada, BorderLayout.NORTH);
+        add(panelDesplazamiento, BorderLayout.CENTER);
+    }
+
+    private void registrarEntradaVehiculo() {
+        String placa = campoPlaca.getText();
+        String tipo = campoTipo.getText();
+        Date horaEntrada = new Date();
+        int numero = numeroVehiculo++;
+
+        Vehiculo vehiculo = new Vehiculo(placa, tipo, horaEntrada, numero);
+        vehiculos.add(vehiculo);
+
+        registrarMensaje("Entrada del vehículo: " + vehiculo);
+
+        campoPlaca.setText("");
+        campoTipo.setText("");
+    }
+
+    private void registrarSalidaVehiculo() {
+        JDialog dialogo = new JDialog(this, "Salida del vehículo: ", true);
+        dialogo.setLayout(new GridLayout(3, 2));
+        JTextField campoPlacaSalida = new JTextField();
+        JButton botonSalida = new JButton("Salir");
+
+        botonSalida.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dialogo.dispose();
+            }
+        });
+
+        dialogo.add(new JLabel("Placa del vehículo retirado:"));
+        dialogo.add(campoPlacaSalida);
+        dialogo.add(new JLabel(""));
+        dialogo.add(botonSalida);
+
+        dialogo.setSize(300, 100);
+        dialogo.setVisible(true);
+
+        String placa = campoPlacaSalida.getText();
+        for (Vehiculo vehiculo : vehiculos) {
+            if (vehiculo.getPlaca().equalsIgnoreCase(placa)) {
+                Date horaSalida = new Date();
+                double costo = vehiculo.calcularCosto(horaSalida);
+                long minutosTotales = vehiculo.calcularTiempoTotal(horaSalida);
+
+                registrarMensaje("Salida del vehículo: " + vehiculo);
+                registrarMensaje("Entrada (Fecha ; Hora): " + vehiculo.getHoraEntrada());
+                registrarMensaje("Salida (Fecha ; Hora) " + horaSalida);
+                registrarMensaje("Tiempo total del vehículo estacionado: " + minutosTotales + " minutos");
+                registrarMensaje("Total a pagar $$: " + costo);
+
+                vehiculos.remove(vehiculo);
+                return;
+            }
+        }
+        registrarMensaje("Vehículo No Encontrado.");
+    }
+
+    private void mostrarListaDosRuedas() {
+        registrarMensaje("Lista de vehículos con 2 ruedas:");
+        for (Vehiculo vehiculo : vehiculos) {
+            if (vehiculo.getTipo().equalsIgnoreCase("Bicicleta") || vehiculo.getTipo().equalsIgnoreCase("Motocicleta")) {
+                registrarMensaje(vehiculo.toString());
+            }
+        }
+    }
+
+    private void mostrarListaCuatroRuedas() {
+        registrarMensaje("Lista de vehículos con 4 ruedas:");
+        double costoTotal = 0;
+        for (Vehiculo vehiculo : vehiculos) {
+            if (!vehiculo.getTipo().equalsIgnoreCase("Bicicleta") && !vehiculo.getTipo().equalsIgnoreCase("Monopatín")) {
+                double costo = vehiculo.calcularCosto(new Date());
+                registrarMensaje(vehiculo.toString() + ", Total a pagar: $" + costo);
+                costoTotal += costo;
+            }
+        }
+        registrarMensaje("Total a pagar (Vehículos de 4 ruedas): $" + costoTotal);
+    }
+
+    private void mostrarTotalVehiculos() {
+        registrarMensaje("Número de vehículos ingresados en el parqueadero : " + vehiculos.size());
+    }
+
+    private void registrarMensaje(String mensaje) {
+        Date ahora = new Date();
+        String marcaTiempo = formatoFecha.format(ahora);
+        areaRegistro.append(marcaTiempo + " - " + mensaje + "\n");
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                AplicacionParqueadero aplicacion = new AplicacionParqueadero();
+                aplicacion.setVisible(true);
+            }
+        });
+    }
+}
+
+class Vehiculo {
+    private String placa;
+    private String tipo;
+    private Date horaEntrada;
+    private int numero;
+
+    public Vehiculo(String placa, String tipo, Date horaEntrada, int numero) {
+        this.placa = placa;
+        this.tipo = tipo;
+        this.horaEntrada = horaEntrada;
+        this.numero = numero;
+    }
+
+    public double calcularCosto(Date horaSalida) {
+        long minutos = (horaSalida.getTime() - horaEntrada.getTime()) / (1000 * 60);
+        if (minutos >= 720) {
+            return 60 * 720;
+        } else {
+            return minutos * 1.0;
+        }
+    }
+
+    public long calcularTiempoTotal(Date horaSalida) {
+        return (horaSalida.getTime() - horaEntrada.getTime()) / (1000 * 60);
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public Date getHoraEntrada() {
+        return horaEntrada;
+    }
+
+    @Override
+    public String toString() {
+        return "Número: " + numero + ", Placa: " + placa + ", Tipo: " + tipo;
+    }
+}
