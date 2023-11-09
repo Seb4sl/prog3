fb264ce728238352f9370eafeeb9fb4cd4fc4c3d
Juan Sebastian Larrota <141367323+Seb4sl@users.noreply.github.com>
2023-11-08 21:39:35 -0500

    Add files via upload

---
+++ labs/06/CarreraAuto/CarreraAuto/src/main/java/com/mycompany/carreraauto/CarreraAuto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.carreraauto;
+
+/**
+ *
+ * @author jlarr
+ */
+public class CarreraAuto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
